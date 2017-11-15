e6d91cd913a275596da64e4abacf13076fb2764f
su-li <deng17032@gmail.com>
2017-11-15 20:34:47 +0800

    测试 commit and push

---
+++ testThreadPool/src/test/java/TestPool.java
@@ -0,0 +1,29 @@
+/**
+ * @author HD
+ * @date 2017/11/15
+ */
+
+public class TestPool {
+
+    public static void main(String[] args) {
+        ThreadPoolUtil pool = ThreadPoolUtil.init();
+        for (int i = 0; i <=1000000; i++) {
+
+            int finalI = i;
+            pool.execute(new Thread() {
+                @Override
+                public void run() {
+                    System.out.println(finalI);
+                }
+            });
+
+        }
+
+        pool.shutdown();
+        try {
+            pool.awaitTermination();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
